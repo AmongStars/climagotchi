648d748ecf9973e8de7910f56b2544c15c424e78
ÄmongStars <ninabarnabishvili.viidakko@gmail.com>
2021-09-18 15:44:28 +0300

    Front page created

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,16 @@
 <template>
-  <Tutorial/>
+  <div class="content">
+    <img src="~/assets/images/logo.svg" class="logo" />
+    <div class="buttons">
+      <b-button variant="player-mode">Player mode</b-button>
+      <b-button variant="teacher-mode">Teacher mode</b-button>
+    </div>
+    <div class="trees">
+      <img src="~/assets/images/trees.png" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
