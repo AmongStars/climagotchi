b7efc3991c73c22202fa38aeb697ae40b0014e4f
ÄmongStars <ninabarnabishvili.viidakko@gmail.com>
2021-09-18 16:27:49 +0300

    Login, signup pages added

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,12 @@
   <div class="content">
     <img src="~/assets/images/logo.svg" class="logo" />
     <div class="buttons">
-      <b-button variant="player-mode">Player mode</b-button>
-      <b-button variant="teacher-mode">Teacher mode</b-button>
+      <b-link to="login">
+        <b-button variant="player-mode">Player mode</b-button></b-link
+      >
+      <b-link to="login"
+        ><b-button variant="teacher-mode">Teacher mode</b-button></b-link
+      >
     </div>
   </div>
 </template>
-
-<script>
-export default {};
-</script>
